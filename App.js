d905b96867cca6f60d585bce81b5c0e03f8a2c7a
AgrowAdmin1 <151200394+AgrowAdmin1@users.noreply.github.com>
2025-02-24 14:11:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import OnlyOfficeFrame from './onlyOffice/OnlyOfficeFrame';
+
+const App = () => {
+  return (
+    <div>
+      <OnlyOfficeFrame/>
+    </div>
+  );
+};
+
+export default App;
+
